4728e6f92a0daf5a6b943cfb3e477678edb56380
Fesaa <77553571+Fesaa@users.noreply.github.com>
2023-09-09 20:23:49 +0200

    Fix merge

--- main.go
+++ main.go
@@ -3,52 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/Fesaa/ical-merger/config"
 	"github.com/Fesaa/ical-merger/ical"
 	"github.com/Fesaa/ical-merger/log"
 	"github.com/Fesaa/ical-merger/server"
 )
-
-var c *config.Config
-var calender string
-
-func updateCache() {
-	now := time.Now()
-	log.Log.Info("One hour since last request, remerging ics files")
-	log.ToWebhook(c.WebHook, "Invalidated cache, remerging ics files")
-	cal, e := ical.Merge(c)
-	if e != nil {
-		log.Log.Error("Error merging ical files", e)
-		log.ToWebhook(c.WebHook, "Error merging ical files: "+e.Error())
-		return
-	}
-	calender = cal.Serialize()
-	log.ToWebhook(c.WebHook, "Merged ical files in "+time.Since(now).String())
-}
-
-func heartbeat() {
-	for range time.Tick(time.Minute * time.Duration(c.Heartbeat)) {
-		updateCache()
-	}
-}
-
-func icsHandler(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-	w.Header().Set("Content-Type", "text/calendar; charset=utf-8")
-	w.Header().Set("Content-Disposition", "attachment; filename=event.ics")
-	_, err := io.Copy(w, strings.NewReader(calender))
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-	}
-	log.Log.Info("Request took", time.Since(now).Milliseconds(), "ms")
-	log.ToWebhook(c.WebHook, "Served ics file in "+time.Since(now).String())
-}
 
 func main() {
 	args := os.Args[1:]
